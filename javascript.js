d4c01d7daacca687464bba6e9958fc56fff99e02
navid ashraf <33981955+naweedashraf@users.noreply.github.com>
2023-03-27 19:39:18 +0330

    add education section

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
 const experience = document.querySelector("#experience-nav");
+const education = document.querySelector("#education-nav");
+const activities = document.querySelector("#activities-nav");
 const home = document.querySelector("#home");
 
 const experienceContent = document.querySelector("#experience");
+const educationContent = document.querySelector("#education");
+const activtiesContent = document.querySelector("#activities");
 
 
 experience.addEventListener("click",() => {
@@ -13,8 +17,28 @@ home.addEventListener("click",() => {
     document.querySelector(".active").className = "";
     home.querySelector("span").className = "active";
 });
+
+education.addEventListener("click",() => {
+    document.querySelector(".active").className = "";
+    education.querySelector("span").className = "active";
+});
+
+activities.addEventListener("click",() => {
+    document.querySelector(".active").className = "";
+    activities.querySelector("span").className = "active";
+});
+
+
 window.addEventListener("scroll", () => {
-    if (scrollY >= experienceContent.offsetTop - 60) {
+    if (scrollY >= activtiesContent.offsetTop - 60) {
+        document.querySelector(".active").className = "";
+        activities.querySelector("span").className = "active";
+    }
+    else if (scrollY >= educationContent.offsetTop - 60) {
+        document.querySelector(".active").className = "";
+        education.querySelector("span").className = "active";
+    }
+    else if (scrollY >= experienceContent.offsetTop - 60) {
         document.querySelector(".active").className = "";
         experience.querySelector("span").className = "active";
     }
